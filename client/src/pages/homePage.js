f72c74c48e08097d1a55ff623ed857b42f5c7a6c
Nakonechnyi-Maksim <nakonechnyi1234567890@gmail.com>
2023-10-16 17:21:07 +0400

    #

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,36 +1,53 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Pizzas from "../mock/mockData";
 import "./homePage.css";
 import Modal from "../modal/pizzaModal";
 
 function HomePage() {
   const [pizzaModalActive, setPizzaModalActive] = useState(false);
+  const [selectPizza, setSelectPizza] = useState(<div></div>);
 
   function handleClick(e) {
+    console.log(e.target.value);
     let data = JSON.parse(e.target.value);
     let dataId = data.id;
-    return localStorage.setItem(dataId, e.target.value);
+    localStorage.setItem(dataId, e.target.value);
   }
+
+  function getPizza(item) {
+    setSelectPizza(item);
+    setPizzaModalActive(true);
+  }
+
   return (
     <div className="menu">
       {Pizzas.map((item) => (
-        <ul className="pizzas">
-          <li>
-            <img src={item.img} onClick={() => setPizzaModalActive(true)} />
-          </li>
-          <li>
-            <h2>{item.name}</h2>
-          </li>
-          <li>{item.ingredients}</li>
-          <li>Размер {item.size} см</li>
-          <li>
-            Цена {item.price}&#8381;{" "}
-            <button value={JSON.stringify(item)} onClick={handleClick}>
-              Добавить
-            </button>
-          </li>
-        </ul>
+        <div>
+          <ul className="pizzas">
+            <li>
+              <img src={item.img} onClick={() => getPizza(item)} />
+            </li>
+            <li>
+              <h2>{item.name}</h2>
+            </li>
+            <li>{item.ingredients}</li>
+            <li>Размер {item.size} см</li>
+            <li>
+              Цена {item.price}&#8381;{" "}
+              <button value={JSON.stringify(item)} onClick={handleClick}>
+                Добавить
+              </button>
+            </li>
+          </ul>
+        </div>
       ))}
+      {selectPizza && (
+        <Modal active={pizzaModalActive} setActive={setPizzaModalActive}>
+          <img src={selectPizza.img} alt="" />
+          <h2>{selectPizza.name}</h2>
+          <p>{selectPizza.ingredients}</p>
+        </Modal>
+      )}
     </div>
   );
 }
