cd474b4050f304de83369129e5ae5ea5d4244d64
Nakonechnyi-Maksim <nakonechnyi1234567890@gmail.com>
2023-10-19 23:02:24 +0400

    #

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -6,15 +6,18 @@ import Modal from "../modal/pizzaModal";
 function HomePage() {
   const [pizzaModalActive, setPizzaModalActive] = useState(false);
   const [selectPizza, setSelectPizza] = useState(<div></div>);
+  const [addButton, setAddButon] = useState(false);
+  const [valueButton, setValueButton] = useState("Добавить");
 
   function handleClick(e) {
     console.log(e.target.value);
     let data = JSON.parse(e.target.value);
     let dataId = data.id;
     localStorage.setItem(dataId, e.target.value);
+    setValueButton("Добавлено");
   }
 
-  function getPizza(item) {
+  function getPizza(item) { 
     setSelectPizza(item);
     setPizzaModalActive(true);
   }
@@ -35,7 +38,7 @@ function HomePage() {
             <li>
               Цена {item.price}&#8381;{" "}
               <button value={JSON.stringify(item)} onClick={handleClick}>
-                Добавить
+                {valueButton}
               </button>
             </li>
           </ul>
